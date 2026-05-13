a4502308d7ddfbf81276729dce72d2f9707a94c4
lukev9k <lukev09000@gmail.com>
2026-05-13 13:29:41 +0530

    wk2: init expo project, add Header component with props

--- App.tsx
+++ App.tsx
@@ -1,9 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+interface HeaderProps {
+  title: string
+}
+
+export function Header({ title }: HeaderProps) {
+  return (
+    <View>
+      <Text>{title}</Text>
+    </View>
+  )
+}
+
 export default function App() {
   return (
     <View style={styles.container}>
+      <Header title = "Kinsou MMA"/>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
